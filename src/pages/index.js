e25e76f32658669fa4fcc19dd25eba20ba357797
Xaver Fleer <xaver.fleer@gmail.com>
2020-08-04 11:50:32 +0200

    Update `about tobias`

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Tobias Bandi</h1>
+    <p>
+      Tobias Bandi schreibt und spricht viel über menschliches Verhalten,
+      Bildung und Erziehung. Er ist Autor von von Artikeln und hält Vorträge auf
+      Konferenzen und an Universitäten sowie vor Elterngruppen und Unternehmen.
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>

f3704cc47bb340a4a031fca47fee0bd662c3bffd
Xaver Fleer <xaver.fleer@gmail.com>
2020-08-07 09:19:41 +0200

    Formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,21 +5,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Tobias Bandi</h1>
-    <p>
-      Tobias Bandi schreibt und spricht viel über menschliches Verhalten,
-      Bildung und Erziehung. Er ist Autor von von Artikeln und hält Vorträge auf
-      Konferenzen und an Universitäten sowie vor Elterngruppen und Unternehmen.
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Tobias Bandi</h1>
+      <p>
+        Tobias Bandi schreibt und spricht viel über menschliches Verhalten,
+        Bildung und Erziehung. Er ist Autor von von Artikeln und hält Vorträge
+        auf Konferenzen und an Universitäten sowie vor Elterngruppen und
+        Unternehmen.
+      </p>
+      <h2>Posts</h2>
+      <div>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go 4to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}

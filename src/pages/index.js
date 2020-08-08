e55852bcdaa7f79fe81c26d7b833da20040497ab
Xaver Fleer <xaver.fleer@gmail.com>
2020-08-08 18:24:33 +0200

    Add lorem ipsum

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,16 @@ export default ({ data }) => {
         Unternehmen.
       </p>
       <h2>Posts</h2>
-      <div>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go 4to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <h3>About things</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </Layout>
   )
 }
